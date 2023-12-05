905223bdedf9431f84e22e5e8b6d2a4142e4ec08
BackupPipizudo <107216530+BackupPipizudo@users.noreply.github.com>
2023-12-05 02:38:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['37254365229', 'Backup Pipizudo', true],
+  ['6281255369012', 'Ash', false], 
   [''] 
 ] //Number of owners
 
